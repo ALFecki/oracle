07ec3431a708ab46f96016f47c7faaf13e15b3d0
Skvortsov A.V <alexskvr03@gmail.com>
2024-04-02 17:32:53 +0300

    create users commands

---
+++ lab3.sql
@@ -0,0 +1,12 @@
+-- CREATE USERS
+
+CREATE USER dev IDENTIFIED BY "root";
+CREATE USER prod IDENTIFIED BY "root";
+CREATE USER lab3 IDENTIFIED BY "root";
+GRANT sysdba TO lab3 container=all;
+
+
+
+
+
+
