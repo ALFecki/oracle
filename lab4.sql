9db1301f6f21a726255f658713a02059c6ef0f4d
ALFecki <alexskvr03@gmail.com>
2024-04-24 12:39:48 +0300

    tests added

--- lab4.sql
+++ lab4.sql
@@ -1,3 +1,5 @@
+-- MAIN TASK
+
 CREATE OR REPLACE NONEDITIONABLE PROCEDURE parse_json_proc(
     json_str CLOB
 ) IS
@@ -356,4 +358,128 @@ BEGIN
         NULL;
     END IF;
 
+END;
+
+-- TEST
+
+DECLARE
+  v_col      VARCHAR2(8000);
+  
+   json_create_t1 CLOB := '{
+      "query_type": "CREATE TABLE",
+      "table": "t1",
+      "columns": [
+         {
+            "name": "id", "type": "NUMBER"
+         },
+         {
+            "name": "num", "type": "NUMBER"
+         },
+         {
+            "name": "val", "type": "VARCHAR2"
+         }
+      ],
+      "primary_keys": ["id"]
+   }';
+   
+
+   json_create_t2 CLOB := '{
+      "query_type": "CREATE TABLE",
+      "table": "t2",
+      "columns": [
+         {
+            "name": "id", "type": "NUMBER"
+         },
+         {
+            "name": "num", "type": "NUMBER"
+         },
+         {
+            "name": "val", "type": "VARCHAR2"
+         },
+         {
+            "name": "t1_k", "type": "NUMBER"
+         }
+         
+      ],
+      "primary_keys": ["id"],
+      "foreign_keys": [{"field": "id", "table": "t1", "ref_field": "id"}]
+   }';
+   
+   json_select_t1_t2 CLOB := '
+   {
+  "query_type": "SELECT",
+  "columns": ["*"],
+  "tables": ["t1"],
+  "join_block":
+  [
+    "RIGHT",
+    "t2",
+    "t2.t1_k = t1.id"
+  ],
+  "filter_conditions": [
+    {
+      "condition_type": "included",
+      "condition": {
+        "query_type": "SELECT",
+        "columns": ["id"],
+        "tables": ["t2"],
+        "filter_conditions": [
+          {"condition": "num between 2 and 4", "operator": "AND"},
+          {"condition": "val like ''%a%''", "operator": "AND"}
+        ],
+        "operator": "IN",
+        "search_col": "t1.id"
+      },
+      "operator": "AND"
+    }
+  ]
+}
+
+   ';
+   
+   json_test_join_select CLOB := '{
+  "query_type": "SELECT",
+  "columns": [
+    "*"
+  ],
+  "tables": [
+    "aaa"
+  ],
+  "join_conditions": [],
+  "filter_conditions": [
+    {
+      "condition_type": "included",
+      "condition": {
+        "query_type": "SELECT",
+        "column": "id",
+        "tables": [
+          "bbb"
+        ],
+        "filter_conditions": [
+          {
+            "condition": "name like ''%''",
+            "operator": "AND"
+          },
+          {
+            "condition": "id between 3 and 5",
+            "operator": "AND"
+          }
+        ],
+        "operator": "NOT IN",
+        "search_col": "id"
+      },
+      "operator": "AND"
+    }
+  ]
+}';
+   
+   
+-- 
+
+BEGIN
+    -- parse_json_proc(json_test_join_select);
+    -- parse_json_proc(json_create_t1);
+    -- parse_json_proc(json_create_t2);
+    parse_json_proc(json_select_t1_t2);
+  
 END;
